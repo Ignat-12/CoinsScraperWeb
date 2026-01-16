60ee8a05944b8624b964dc203e82ecefd815a144
Ignat-12 <vijkd12@gmail.com>
2026-01-16 14:21:49 +0200

    Add portfolio visualisation

--- coinsscraper/portfolio/models.py
+++ coinsscraper/portfolio/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=255)
+    own_wallet = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Asset(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        related_name="assets",
+        on_delete=models.CASCADE,
+    )
+
+    cryptocurrency_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=20)
+
+    amount = models.FloatField()
+    current_price = models.FloatField()
+    holdings_value = models.FloatField()
+
+    pl_value = models.FloatField()
+    pl_percent_value = models.FloatField()
+
+    last_updated = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} ({self.symbol})"
--- coinsscraper/portfolio/views.py
+++ coinsscraper/portfolio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from coinsscraper.portfolio.models import Portfolio
+
+
+def portfolio_list(request):
+    portfolios = Portfolio.objects.prefetch_related("assets")
+    return render(
+        request,
+        "portfolios/portfolio_list.html",
+        {"portfolios": portfolios},
+    )
