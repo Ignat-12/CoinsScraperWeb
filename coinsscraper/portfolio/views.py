30353ba29d3a414179b512ded6423d4ad7924e37
Ignat-12 <vijkd12@gmail.com>
2026-01-18 16:03:30 +0200

    Add portfolio models, view and template

--- coinsscraper/portfolio/views.py
+++ coinsscraper/portfolio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Portfolio
+
+
+def portfolio_list(request):
+    portfolios = Portfolio.objects.prefetch_related("assets")
+    return render(
+        request,
+        "portfolio/portfolio_list.html",
+        {"portfolios": portfolios},
+    )
